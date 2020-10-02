e76b1934f597224f5e2e734a2c2ecbf88439e18b
Stéphane Guerrier <stephane.guerrier@unige.ch>
2020-10-02 21:13:00 +0200

    Incorporating Christoph's comments
    - Adding inputs checks
    - Fix bug on print of survey_mle
    - Chaging inputs (n is no longer needed) for conditional_mle function
    - Minor update on README

--- R/estimation.R
+++ R/estimation.R
@@ -20,7 +20,7 @@
 #'  \item ...:         Additional parameters
 #' }
 #' @export
-#' @author Stephane Guerrier
+#' @author Stephane Guerrier, Maria-Pia Victoria-Feser, Christoph Kuzmics
 #' @examples
 #' # Samples without measurement error
 #' X = sim_Rs(theta = 30/1000, pi0 = 10/1000, n = 1500, seed = 18)
@@ -33,6 +33,31 @@
 #' survey_mle(R = X$R, n = X$n, alpha = 0.01, beta = 0.05)
 #' @importFrom stats qbeta qnorm
 survey_mle = function(R, n, pi0 = 0, alpha = 0, beta = 0, gamma = 0.05, ...){
+  # Check inputs
+  if (R%%1!=0 || R < 0){
+    stop("R should be non-negative integer.")
+  }
+
+  if (n%%1!=0 || n < 0){
+    stop("n should be non-negative integer.")
+  }
+
+  if (R > n){
+    stop("R and n should be such that R <= n.")
+  }
+
+  if (pi0 < 0 || pi0 > 1){
+    stop("pi0 should be such that 0 <= pi0 <= 1.")
+  }
+
+  if (alpha < 0 || alpha >= 1 || beta < 0 || beta >= 1 || alpha + beta > 1){
+    stop("alpha and beta should be such that: (i) 0 <= alpha < 1, (ii) 0 <= beta < 1, (iii) alpha + beta < 1.")
+  }
+
+  if (gamma < 0 || gamma > 1){
+    stop("gamma should be between 0 and 1.")
+  }
+
   # Compute survey proportion
   pi_bar = R/n
 
@@ -88,7 +113,7 @@ survey_mle = function(R, n, pi0 = 0, alpha = 0, beta = 0, gamma = 0.05, ...){
 
   # Construct output
   out = list(estimate = pi_bar, sd = sd, ci_asym = ci_asym, ci_cp = ci_cp, gamma = gamma,
-             method = "Survey sample", measurement = c(NA, alpha, NA, beta))
+             method = "Survey MLE", measurement = c(NA, alpha, NA, beta))
   class(out) = "cpreval"
   out
 }
@@ -176,7 +201,7 @@ print.cpreval = function(x, ...){
 #'  \item ...:         Additional parameters
 #' }
 #' @export
-#' @author Stephane Guerrier
+#' @author Stephane Guerrier, Maria-Pia Victoria-Feser, Christoph Kuzmics
 #' @examples
 #' # Samples without measurement error
 #' X = sim_Rs(theta = 3/100, pi0 = 1/100, n = 1500, seed = 18)
@@ -190,6 +215,35 @@ print.cpreval = function(x, ...){
 #' alpha = 0.01, beta0 = 0.05, beta = 0.05)
 #' @importFrom stats qbeta qnorm
 moment_estimator = function(R3, n, pi0, gamma = 0.05, alpha = 0, beta = 0, alpha0 = 0, beta0 = 0, ...){
+  # Check inputs
+  if (R3%%1!=0 || R3 < 0){
+    stop("R3 should be non-negative integer.")
+  }
+
+  if (n%%1!=0 || n < 0){
+    stop("n should be non-negative integer.")
+  }
+
+  if (R3 > n){
+    stop("R3 and n should be such that R3 <= n.")
+  }
+
+  if (pi0 < 0 || pi0 > 1){
+    stop("pi0 should be such that 0 <= pi0 <= 1.")
+  }
+
+  if (alpha < 0 || alpha >= 1 || beta < 0 || beta >= 1 || alpha + beta > 1){
+    stop("alpha and beta should be such that: (i) 0 <= alpha < 1, (ii) 0 <= beta < 1, (iii) alpha + beta < 1.")
+  }
+
+  if (alpha0 < 0 || alpha0 >= 1 || beta0 < 0 || beta0 >= 1 || alpha0 + beta0 > 1){
+    stop("alpha and beta should be such that: (i) 0 <= alpha0 < 1, (ii) 0 <= beta0 < 1, (iii) alpha0 + beta0 < 1.")
+  }
+
+  if (gamma < 0 || gamma > 1){
+    stop("gamma should be between 0 and 1.")
+  }
+
   # Compute point estimate
   Delta = 1 - alpha - beta
   Delta0 = 1 - alpha0 - beta0
@@ -257,7 +311,6 @@ neg_log_lik = function(theta, Rvect, n, pi0, alpha, beta, alpha0, beta0, ...){
 }
 
 
-
 #' @title Negative marginalized log-likelihood function based on R0 and R
 #' @description Log-Likelihood function based on R1 and R3 multiplied by -1.
 #' @param theta     A \code{numeric} value for the paramerer of interest.
@@ -284,7 +337,7 @@ neg_log_lik_integrated = function(theta, Rvect, n, pi0, alpha, beta, alpha0, bet
 #' @param R2        A \code{numeric} that provides the number of participants in the survey sample that are tested positive only with the first testing device (and are, thus,  members of the sub-population).
 #' @param R3        A \code{numeric} that provides the number of participants in the survey sample that are tested positive only with the second testing device.
 #' @param R4        A \code{numeric} that provides the number of participants that are tested negative with the second testing device (and are either members of the sub-population and have tested negative with the first testing device or are not members of the sub-population).
-#' @param n         A \code{numeric} that provides the sample size.
+#' @param n         A \code{numeric} that provides the sample size. Default value R1 + R2 + R3 + R4. If this value is provided it is used to verify that R1 + R2 + R3 + R4 = n.
 #' @param pi0       A \code{numeric} that provides the prevalence or proportion of people (in the whole population) who are positive, as measured through a non-random,
 #' but systematic sampling (e.g. based on medical selection).
 #' @param gamma     A \code{numeric} that used to compute a (1 - gamma) confidence region for the proportion. Default value is \code{0.05}.
@@ -304,20 +357,101 @@ neg_log_lik_integrated = function(theta, Rvect, n, pi0, alpha, beta, alpha0, bet
 #'  \item ...:         Additional parameters
 #' }
 #' @export
-#' @author Stephane Guerrier
+#' @author Stephane Guerrier, Maria-Pia Victoria-Feser, Christoph Kuzmics
 #' @examples
 #' # Samples without measurement error
 #' X = sim_Rs(theta = 3/100, pi0 = 1/100, n = 1500, seed = 18)
-#' conditional_mle(R1 = X$R1, R2 = X$R2, R3 = X$R3, R4 = X$R4, n = X$n, pi0 = X$pi0)
+#' conditional_mle(R1 = X$R1, R2 = X$R2, R3 = X$R3, R4 = X$R4, pi0 = X$pi0)
 #'
 #' # With measurement error
 #' X = sim_Rs(theta = 30/1000, pi0 = 10/1000, n = 1500, alpha0 = 0.01,
 #' alpha = 0.01, beta0 = 0.05, beta = 0.05, seed = 18)
-#' conditional_mle(R1 = X$R1, R2 = X$R2, R3 = X$R3, R4 = X$R4, n = X$n, pi0 = X$pi0)
-#' conditional_mle(R1 = X$R1, R2 = X$R2, R3 = X$R3, R4 = X$R4, n = X$n, pi0 = X$pi0,
+#' conditional_mle(R1 = X$R1, R2 = X$R2, R3 = X$R3, R4 = X$R4, pi0 = X$pi0)
+#' conditional_mle(R1 = X$R1, R2 = X$R2, R3 = X$R3, R4 = X$R4, pi0 = X$pi0,
 #' alpha0 = 0.01, alpha = 0.01, beta0 = 0.05, beta = 0.05)
 #' @importFrom stats optimize qnorm
-conditional_mle = function(R1, R2, R3, R4, n, pi0, gamma = 0.05, alpha0 = 0, alpha = 0, beta0 = 0, beta = 0, ...){
+conditional_mle = function(R1 = NULL, R2 = NULL, R3 = NULL, R4 = NULL, n = R1 + R2 + R3 + R4, pi0, gamma = 0.05, alpha0 = 0, alpha = 0, beta0 = 0, beta = 0, ...){
+  # Check inputs
+  if (is.null(R1) + is.null(R2) + is.null(R3) + is.null(R4) == 1 && length(n) == 0){
+    if (is.null(R1)){
+      stop("R1 is missing.")
+    }
+
+    if (is.null(R2)){
+      stop("R2 is missing.")
+    }
+
+    if (is.null(R3)){
+      stop("R3 is missing.")
+    }
+
+    if (is.null(R4)){
+      stop("R4 is missing.")
+    }
+  }
+
+  if (is.null(R1) + is.null(R2) + is.null(R3) + is.null(R4) + is.null(n) == 1){
+    if (is.null(R1)){
+      R1 = n - R2 - R3 - R4
+      warning(paste("R1 was computed using the other values: R1 = ", R1, sep = ""))
+    }
+
+    if (is.null(R2)){
+      R2 = n - R1 - R3 - R4
+      warning(paste("R2 was computed using the other values: R2 = ", R2, sep = ""))
+    }
+
+    if (is.null(R3)){
+      R3 = n - R1 - R2 - R4
+      warning(paste("R3 was computed using the other values: R3 = ", R3, sep = ""))
+    }
+
+    if (is.null(R4)){
+      R4 = n - R1 - R2 - R3
+      warning(paste("R4 was computed using the other values: R4 = ", R4, sep = ""))
+    }
+  }
+
+  if (R1%%1!=0 || R1 < 0){
+    stop("R1 should be non-negative integer.")
+  }
+
+  if (R2%%1!=0 || R2 < 0){
+    stop("R2 should be non-negative integer.")
+  }
+
+  if (R3%%1!=0 || R3 < 0){
+    stop("R3 should be non-negative integer.")
+  }
+
+  if (R4%%1!=0 || R4 < 0){
+    stop("R4 should be non-negative integer.")
+  }
+
+  if (n%%1!=0 || n < 0){
+    stop("n should be non-negative integer.")
+  }
+
+  if (R1 + R2 + R3 + R4 != n){
+    stop("The sum of R_i should be equal to n.")
+  }
+
+  if (pi0 < 0 || pi0 > 1){
+    stop("pi0 should be such that 0 <= pi0 <= 1.")
+  }
+
+  if (alpha < 0 || alpha >= 1 || beta < 0 || beta >= 1 || alpha + beta > 1){
+    stop("alpha and beta should be such that: (i) 0 <= alpha < 1, (ii) 0 <= beta < 1, (iii) alpha + beta < 1.")
+  }
+
+  if (alpha0 < 0 || alpha0 >= 1 || beta0 < 0 || beta0 >= 1 || alpha0 + beta0 > 1){
+    stop("alpha and beta should be such that: (i) 0 <= alpha0 < 1, (ii) 0 <= beta0 < 1, (iii) alpha0 + beta0 < 1.")
+  }
+
+  if (gamma < 0 || gamma > 1){
+    stop("gamma should be between 0 and 1.")
+  }
+
   # Find MLE (TODO use closed form when possible)
   R = c(R1, R2, R3, R4)
   eps = 10^(-5)
@@ -388,7 +522,7 @@ conditional_mle = function(R1, R2, R3, R4, n, pi0, gamma = 0.05, alpha0 = 0, alp
 #'  \item ...:         Additional parameters
 #' }
 #' @export
-#' @author Stephane Guerrier
+#' @author Stephane Guerrier, Maria-Pia Victoria-Feser, Christoph Kuzmics
 #' @examples
 #' # Samples without measurement error
 #' X = sim_Rs(theta = 3/100, pi0 = 1/100, n = 1500, seed = 18)
@@ -402,6 +536,39 @@ conditional_mle = function(R1, R2, R3, R4, n, pi0, gamma = 0.05, alpha0 = 0, alp
 #' alpha0 = 0.01, alpha = 0.01, beta0 = 0.05, beta = 0.05)
 #' @importFrom stats optimize
 marginal_mle = function(R1, R3, n, pi0, gamma = 0.05, alpha = 0, beta = 0, alpha0 = 0, beta0 = 0, ...){
+  # Check inputs
+  if (R1%%1!=0 || R1 < 0){
+    stop("R1 should be non-negative integer.")
+  }
+
+  if (R3%%1!=0 || R3 < 0){
+    stop("R3 should be non-negative integer.")
+  }
+
+  if (n%%1!=0 || n < 0){
+    stop("n should be non-negative integer.")
+  }
+
+  if (R1 + R3 > n){
+    stop("R1 + R3 can't be larger than n.")
+  }
+
+  if (pi0 < 0 || pi0 > 1){
+    stop("pi0 should be such that 0 <= pi0 <= 1.")
+  }
+
+  if (alpha < 0 || alpha >= 1 || beta < 0 || beta >= 1 || alpha + beta > 1){
+    stop("alpha and beta should be such that: (i) 0 <= alpha < 1, (ii) 0 <= beta < 1, (iii) alpha + beta < 1.")
+  }
+
+  if (alpha0 < 0 || alpha0 >= 1 || beta0 < 0 || beta0 >= 1 || alpha0 + beta0 > 1){
+    stop("alpha and beta should be such that: (i) 0 <= alpha0 < 1, (ii) 0 <= beta0 < 1, (iii) alpha0 + beta0 < 1.")
+  }
+
+  if (gamma < 0 || gamma > 1){
+    stop("gamma should be between 0 and 1.")
+  }
+
   # Compute MLE (TODO use closed form when possible)
   R = c(R1, NA, R3, NA)
   eps = 10^(-5)
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ status](https://travis-ci.com/stephaneguerrier/cape.svg?branch=master)](https://
 [![Licence](https://img.shields.io/badge/licence-AGPL--3.0-blue.svg)](https://opensource.org/licenses/AGPL-3.0)
 [![minimal R
 version](https://img.shields.io/badge/R%3E%3D-3.6.0-6666ff.svg)](https://cran.r-project.org/)
-[![Last-changedate](https://img.shields.io/badge/last%20change-2020--09--29-green.svg)](https://github.com/stephaneguerrier/cape)
+[![Last-changedate](https://img.shields.io/badge/last%20change-2020--10--02-green.svg)](https://github.com/stephaneguerrier/cape)
 [![CRAN
 status](https://www.r-pkg.org/badges/version/stacks)](https://CRAN.R-project.org/package=stacks)
 [![lifecycle](https://img.shields.io/badge/lifecycle-experimental-blue.svg)](https://www.tidyverse.org/lifecycle/#experimental)
@@ -33,6 +33,10 @@ install.packages("devtools")
 # Install the package from GitHub
 devtools::install_github("stephaneguerrier/cape")
 ```
+
+Note that Windows users are assumed that have Rtools installed (if this
+is not the case, please visit this
+[link](https://cran.r-project.org/bin/windows/Rtools/)).
 
 # How to cite
 
