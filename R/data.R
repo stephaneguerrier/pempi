9b49b68fa41d0833e701b8e83c6cafe607d160ed
Stéphane Guerrier <stephane.guerrier@unige.ch>
2022-05-01 18:47:36 +0200

    Add data

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
-#' COVID-19 Data from Statistics Austria
+#' @title COVID-19 Data from Statistics Austria
 #'
-#' Data collected in Austria in 2020 (see e.g. SORA, 2020; Kowarik et al., 2021, for more details),
+#' @description Data collected in Austria in 2020 (see e.g. SORA, 2020; Kowarik et al., 2021, for more details),
 #' allowing to COVID-19 prevalence.
 #'
 #' @format A \code{matrix} with 2290 rows and 3 variables:
@@ -9,5 +9,5 @@
 #'   \item{Z}{Binary variable, 1 if participant i was declared positive with the official procedure, 0 otherwise.}
 #'   \item{weights}{Sampling weights.}
 #' }
-#' @source Statistics Austria. 2020. “Prävalenz von SARS-CoV-2-Infektionen liegt bei 3,1%.” \url{https://www.statistik.at/web_de/presse/124846.html}
+#' @source Statistics Austria. 2020. "Prävalenz von SARS-CoV-2-Infektionen liegt bei 0.031." \url{https://www.statistik.at/web_de/presse/124846.html}
 "covid19_austria"
